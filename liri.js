0d713af9309bbfab3133d9c5d182d1c808d95941
ZhAYinG <8896093+zhaying@users.noreply.github.com>
2018-06-30 17:48:33 -0400

    added some more logic for options of user selection

--- liri.js
+++ liri.js
@@ -12,6 +12,7 @@ var client = new Twitter(keys.twitter);
 
 // variable for user selection
 var userArg = {
+  userOption: null,
   userSelection: null
 };
 
@@ -29,16 +30,35 @@ console.log("\nWelcome to LIRI!");
 //Check for argument after program name
 if (process.argv[2]) {
   userArg.userSelection = process.argv[2];
+  console.log(" userArg.userOption=",userArg.userOption);
+}
+if (process.argv[3]) {
+  userArg.userOption = process.argv[3];
   console.log(" userArg.userSelection=",userArg.userSelection);
 }
 
 //Check for command use
 if (userArg.userSelection === commands.twitterCMD) {
   console.log("\nLoading twitter");
+  //query twitter API
+  // write list of tweets to file
+  // return list of tweets to console
+
 } else if (userArg.userSelection === commands.spotifyCMD) {
+  // Describe command used
   console.log("\nLoading spotify");
+  // Check for options used with command
+  if (userArg.userOption) {
+    console.log("\n spotify with options");
+  }
+  //query a song from spotify API
+  // return song info to console
+
 } else if (userArg.userSelection === commands.omdbCMD) {
   console.log("\nLoading omdb");
+  //query omdb for a movie list
+  // return movie list to console
 } else if (userArg.userSelection === commands.meCMD) {
   console.log("\nLoading me");
+
 }
